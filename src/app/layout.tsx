6b57f6af57c83ab21c5d39fb2e2ba4eac5981d78
sealand-logistics <sealandshipping.bbroy@gmail.com>
2026-08-08 15:03:21 +0530

    feat: add Google Analytics (gtag.js) tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit, Cormorant_Garamond } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -88,11 +89,29 @@ export default function RootLayout({
     ]
   };
 
+  const gaId = process.env.NEXT_PUBLIC_GA_ID || "G-5N7V0B3X6";
+
   return (
     <html lang="en" className="scroll-smooth">
       <body
         className={`${outfit.variable} ${cormorant.variable} antialiased bg-[#f8fcf3] text-[#111111]`}
       >
+        {gaId && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaId}');
+              `}
+            </Script>
+          </>
+        )}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
